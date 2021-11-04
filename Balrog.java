6c0e595160962f98abe2ec86d9759d7478f1537a
MJV314 <71029215+Pi-Guy53@users.noreply.github.com>
2021-11-04 18:45:59 -0400

    added the Balrog, seems a bit OP, and I don't like that any excess damage from it is lost. It is lost for any creature, but the Balrog would generate a lot of excess damage

---
+++ Balrog.java
@@ -0,0 +1,37 @@
+
+/**
+ * The Balrog is a massive fire Demon
+ * Min/Max Strength 50/100
+ * Min/Max Hit Points 80/200
+ *
+ * @author Mihail Vaporakis
+ * @version 2021-11-4
+ */
+public class Balrog extends Demon
+{
+    private static final int MAX_BALROG_HP = 200;
+    private static final int MIN_BALROG_HP = 80;
+    private static final int MAX_BALROG_STR = 100;
+    private static final int MIN_BALROG_STR = 50;
+
+    /**
+     * Generates a Demon with Blarog Stats
+     */
+    public Balrog()
+    {
+        super(
+            Randomizer.nextInt(MAX_BALROG_HP-MIN_BALROG_HP)+MIN_BALROG_HP,    
+            Randomizer.nextInt(MAX_BALROG_STR-MIN_BALROG_STR)+MIN_BALROG_STR);
+    }
+    
+    /**
+     * Overridden Attack method allowing the Balrog to attack twice
+     * @return damage inflicted
+     */
+    public int attack(){
+        int dmg = super.attack();
+        dmg = dmg + super.attack();
+        
+        return dmg;
+    }
+}
