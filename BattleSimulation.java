a3e609409c6b2e39a55d1bc542055e506c3baa4d
MJV314 <71029215+Pi-Guy53@users.noreply.github.com>
2021-11-05 14:01:36 -0400

    Added methods to create the armies to BattleSimulation,

--- BattleSimulation.java
+++ BattleSimulation.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 /**
  * This is a simulation of a battle that takes place between two massive armies
  * Humans and Elves are on one side, facing off against Humans and Demons.
@@ -7,14 +8,96 @@
  */
 public class BattleSimulation
 {
-    private Creature[] armyOne;
-    private Creature[] armyTwo;
-    
-    private int armyOneIndex;
-    private int armyTwoIndex;
-    
+    //ArrayList of creatures to form the army
+    private ArrayList<Creature> armyOne;
+    private ArrayList<Creature> armyTwo;
+
+    //Index counter for the armies
+    private int armyOneIndex = 0;
+    private int armyTwoIndex = 0;
+
+    //The size of the individual armies
+    private int armyOneSize;
+    private int armyTwoSize;
+
+    //Max and min army sizes
+    private int armyOneMaxSize = 110;
+    private int armyOneMinSize = 90;
+    private int armyTwoMaxSize = 50;
+    private int armyTwoMinSize = 30;
+    /**
+     * Constructor method for BattleSimulation, inisilizes two armies of
+     * random composition
+     */
     public BattleSimulation()
+    {
+        armyOneSize = Randomizer.nextInt((armyOneMaxSize - armyOneMinSize) + armyOneMinSize);
+        armyTwoSize = Randomizer.nextInt((armyTwoMaxSize - armyTwoMinSize) + armyTwoMinSize);
+        
+        for(int i = 0; i < armyOneSize; i++)
+        {
+            armyOne.add(addToArmyOne());
+        }
+        
+        for(int i = 0; i < armyTwoSize; i++)
+        {
+            armyTwo.add(addToArmyTwo());
+        }
+        
+        Fight();
+    }
+
+    /**
+     * Fighting method, loops until one army is defeated
+     */
+    public void Fight()
     {
         
     }
+    
+    /**
+     * Generates a random Creature to add to armyOne
+     * @return the creature to be added
+     */
+    private Creature addToArmyOne()
+    {
+        Creature warrior;
+
+        if(Randomizer.nextInt(10) <= 6)
+        {
+            warrior = new Human();
+        }
+        else 
+        {
+            warrior = new Elf();
+        }
+
+        return warrior;
+    }
+
+    /**
+     * Generates a random creature for armyTwo
+     * @return the creature to be added
+     */
+    private Creature addToArmyTwo()
+    {
+        Creature warrior;
+        int r = Randomizer.nextInt(25);
+
+        if(r <= 18)
+        {
+            warrior = new Human();
+        }
+        else if(r <= 24)
+        {
+            warrior = new CyberDemon();
+        }
+        else
+        {
+            warrior = new Balrog();
+        }
+
+        return warrior;
+    }
+
 }
